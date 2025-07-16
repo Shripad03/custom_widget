6d970b41db465e4afa07962c9cd6ce78e94fd157
Shripad03 <41938325+Shripad03@users.noreply.github.com>
2025-07-16 13:48:42 +0530

    Add Metric Radio Button Popup

--- src/app/components/widget-settings/widget-settings.component.ts
+++ src/app/components/widget-settings/widget-settings.component.ts
@@ -2,6 +2,7 @@ import { Component, Inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA, MatDialogModule } from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
@@ -86,7 +87,8 @@ export class WidgetSettingsComponent implements OnInit {
   constructor(
     public dialogRef: MatDialogRef<WidgetSettingsComponent>,
     @Inject(MAT_DIALOG_DATA) public data: { widget: Widget },
-    private dataService: DataService
+    private dataService: DataService,
+    private dialog: MatDialog
   ) {
     this.widget = { ...data.widget };
     this.dataSources$ = this.dataService.getDataSources();
